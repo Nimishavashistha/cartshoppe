171af292786a1aad1dc8cddd0396bae6750625bb
Nimishavashistha <nimishavasistha@gmail.com>
2021-08-11 23:37:01 +0530

    some html files added

--- cartshoppe/settings.py
+++ cartshoppe/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#  managing media
+MEDIA_URL = '/image/download/'
+MEDIA_ROOT = BASE_DIR
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
