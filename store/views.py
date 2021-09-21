7d72d212e712b6b0db48c0beb2454b576ab504c1
Nimishavashistha <nimishavasistha@gmail.com>
2021-09-21 23:45:18 +0530

    customer model created

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models.product import Product
 from .models.category import Category
+from .models.customer import Customer
+from store.models import customer
 
 # Create your views here.
 def index(request):
@@ -16,4 +18,26 @@ def index(request):
     data['products'] = products
     data['categories'] = categories
     return render(request, 'index.html', data)
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    else:
+        postData = request.POST
+        first_name = postData.get('first_name')
+        last_name = postData.get('last_name')
+        email = postData.get('email')
+        password = postData.get('password')
+        phone = postData.get('phone')
+        customer = Customer(first_name=first_name,
+        last_name=last_name,
+        phone=phone,
+        email=email,
+        password=password
+        )
+        customer.register()
+        return HttpResponse("signup success")
+
+        
 
