8d438465a0343a3041beabe5ca3ab4bec2cf3d79
Nimishavashistha <nimishavasistha@gmail.com>
2022-01-06 23:01:34 +0530

    adding products to card method implemented

--- store/views/home.py
+++ store/views/home.py
@@ -6,6 +6,9 @@ from django.views import View
 
 class Index(View):
     def get(self, request):
+        cart = request.session.get('cart')
+        if not cart:
+            request.session['cart'] = {}
         products = None
         categories = Category.get_all_categories()
         categoryId = request.GET.get('category')
@@ -16,12 +19,31 @@ class Index(View):
         data = {}
         data['products'] = products
         data['categories'] = categories
-        print(request.session.get('email'))
+        # print(request.session.get('email'))
         return render(request, 'index.html', data)
     
     def post(self, request):
         product = request.POST.get('product')
-        print(product)
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                  if quantity <= 1:
+                      cart.pop(product)
+                  else:
+                      cart[product] = quantity - 1
+                else:
+                  cart[product] = quantity + 1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+            
+        request.session['cart'] = cart
+        print(request.session['cart'])
         return redirect('homepage')
         
            
