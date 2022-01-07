ba469d5b2519becbfdeba79c4710c960c58ba0ca
Nimishavashistha <nimishavasistha@gmail.com>
2022-01-07 21:12:26 +0530

    fetching products from cart

--- store/views/login.py
+++ store/views/login.py
@@ -15,14 +15,19 @@ class Login(View):
         if customer:
             flag = check_password(password, customer.password)
             if flag:
-                request.session['customer_id'] = customer.id
-                request.session['email'] = customer.email
+                request.session['customer'] = customer.id
+                
                 return redirect('homepage')
             else:
                 error_msg = 'Email or password invalid !!'
         else:
             error_msg = 'Email or password invalid !!'
         return render(request, 'login.html', {'error': error_msg})
+    
+
+def logout(request):
+    request.session.clear()
+    return redirect('login')
 
 
 
