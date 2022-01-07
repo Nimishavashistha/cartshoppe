e4eababd2600e91f672dce0852878c68f05960a7
Nimishavashistha <nimishavasistha@gmail.com>
2022-01-07 22:05:06 +0530

    showing products in cart page

--- store/views/cart.py
+++ store/views/cart.py
@@ -8,6 +8,6 @@ class Cart(View):
     def get(self, request):
         ids = list(request.session.get('cart').keys())
         products = Product.get_products_by_id(ids)
-        return render(request,'cart.html')
+        return render(request,'cart.html', {'products': products})
     
     
